2e3dce7430d9008dcaf0b700d48e0a46293deae7
marcodes-csc <>
2026-06-27 01:20:06 +0000

    Arithmetic Operators

---
+++ basic_types_and_operations/arithmetic_operators/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 9;
+        int secondNumber = 4;
+        double thirdNumber = 2.0;
+        // Use variables instead of hardcoded values
+        double result = (firstNumber + secondNumber) / thirdNumber;
+        System.out.println(result);
+    }
+}
